effd53c0831a1e232a3b82e721125ebf23ea5cdc
ChenGiuseppe <giuseppechen05@gmail.com>
2021-11-12 16:30:44 +0100

    More accurate masses for Z2mass

--- pymm/conversions.py
+++ pymm/conversions.py
@@ -2,8 +2,18 @@
     to and from a.u.)'''
 
 # Dictionaries to help converting between different formats
-Z2mass = {'1': 1.0, '6': 12.0, '7': 14.0, '8': 16.0, '16': 32.0}
-mass2symbol = {1: 'H', 12: 'C', 14: 'N', 16: 'O', 32: 'S'}
+Z2mass = {'1': 1.00784,
+          '6': 12.0107,
+          '7': 14.0067,
+          '8': 15.999,
+          '16': 32.065}
+
+mass2symbol = {1: 'H',
+               12: 'C',
+               14: 'N',
+               16: 'O',
+               32: 'S'}
+
 symbol2mass = {v: k for k, v in mass2symbol.items()}
 
 # Units convertion
@@ -12,4 +22,4 @@ Debye2au = 0.393456
 Bohr2Ang = 0.529177249
 
 if __name__ == '__main__':
-    print(mass2symbol[32])
+    pass
