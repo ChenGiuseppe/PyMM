b7c54ccc4120d9ae3d1699da30f3d49e5909748d
Giuseppe Chen <giuseppechen05@gmail.com>
2022-05-19 12:20:26 +0200

    Added license and readme to setup. Now ver 1.0

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from os import name
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name      = 'PyMM',
-    version   = '0.0.2a1',
+    version   = '1.0',
+    long_description=readme,
+    license=license,
+    url='https://github.com/ChenGiuseppe/PyMM',
     packages  = find_packages(),
     entry_points= {
         'console_scripts': ['pymm=pymm.pymm:main']
